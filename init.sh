eaad82a74333ba6bbd349f915e6e2a6bcedf544e
jason-luo <luo.jian@outlook.com>
2019-02-10 23:02:48 +0800

    Update init.sh
    修正几个问题：
    1. 对于非ubuntu系统，不需要apt这个操作
    2. 下载plug.vim的路径有问题
    3. 不需要创建.vimrc的链接，vim可以找到vimrc

--- init.sh
+++ init.sh
@@ -2,19 +2,21 @@
 
 basepath=$(pushd `dirname $0` > /dev/null; pwd; popd > /dev/null)
 
-if [ ! -f "/etc/yum.repos.d/vim-latest.repo" ];then
-    sudo curl -L https://copr.fedorainfracloud.org/coprs/lantw44/vim-latest/repo/epel-7/lantw44-vim-latest-epel-7.repo -o /etc/yum.repos.d/vim-latest.repo
-    sudo yum update -n
-    sudo yum install vim-enhanced global-ctags ShellCheck -y
-fi
+# i use arch linux, no yum:
+# pacman -S vim ctags 
+#if [ ! -f "/etc/yum.repos.d/vim-latest.repo" ];then
+#    sudo curl -L https://copr.fedorainfracloud.org/coprs/lantw44/vim-latest/repo/epel-7/lantw44-vim-latest-epel-7.repo -o /etc/yum.repos.d/vim-latest.repo
+#    sudo yum update -n
+#    sudo yum install vim-enhanced global-ctags ShellCheck -y
+#fi
 
 if [ ! -f "${basepath}/autoload/plug.vim" ];then
-    curl -fLo ${basepath}/vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    curl -fLo ${basepath}/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 
 rm -fr ~/.vim
 ln -sf ${basepath} ~/.vim
-ln -sf ~/.vim/vimrc ~/.vimrc
+#ln -sf ~/.vim/vimrc ~/.vimrc
 
 vim << EOF
 :PlugUpdate
